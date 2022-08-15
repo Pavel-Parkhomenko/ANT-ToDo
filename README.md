cfa7a6d84e72d3921373ed4c6e17a449398c9f84
Pavel Parkhomenka <pawelparhomenko@gmail.com>
2022-08-15 17:25:51 +0300

    init: create project

---
+++ README.md
@@ -0,0 +1,1 @@
+Ant test app to do list
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <Button type="primary">Button</Button>
+  )
+}
+
+export default App
